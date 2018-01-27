65eef191cf259728200fccab740377fc71dd514a
Viktor Smari <viktorsmari@gmail.com>
2018-01-27 18:18:58 +0100

    First test cases of Roman numbers

--- lib/roman.rb
+++ lib/roman.rb
@@ -1,6 +1,33 @@
 class Roman
+  attr_accessor :total
 
-  def meth
-    puts '______'
+  def initialize
+    @total = 0
+  end
+
+  def convert_one(letter)
+    case letter
+    when 'I'
+      return 1
+    when 'V'
+      return 5
+    when 'X'
+      return 10
+    when 'L'
+      return 50
+    when 'C'
+      return 100
+    when 'D'
+      return 500
+    when 'M'
+      return 1000
+    end
+  end
+
+  def rom_to_num(input)
+    input.each_char do |i|
+      @total += convert_one(i).to_i
+    end
+    @total
   end
 end
--- spec/roman_spec.rb
+++ spec/roman_spec.rb
@@ -1,10 +1,24 @@
 require 'roman'
-describe "ddddd" do
+describe "Roman number" do
   before do
     @r = Roman.new
   end
-  it "runs" do
-    p '--------------'
-    @r.meth
+
+  it "should get simple numbers" do
+    expect(Roman.new.rom_to_num('I')).to eq (1)
+    expect(Roman.new.rom_to_num('V')).to eq (5)
+    expect(Roman.new.rom_to_num('X')).to eq (10)
+    expect(Roman.new.rom_to_num('L')).to eq (50)
+    expect(Roman.new.rom_to_num('C')).to eq (100)
+    expect(Roman.new.rom_to_num('D')).to eq (500)
+    expect(Roman.new.rom_to_num('M')).to eq (1000)
+  end
+  it "should get multiple simple legal letter ordering" do
+    expect(Roman.new.rom_to_num('VIII')).to eq (8)
+    expect(Roman.new.rom_to_num('MIII')).to eq (1003)
+  end
+
+  it "should be able to withdraw negative number" do
+    #expect(Roman.new.rom_to_num('IV')).to eq (4)
   end
 end
